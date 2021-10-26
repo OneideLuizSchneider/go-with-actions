5788b9b182153a9093c5c24b60459ebfcff2c4f9
Oneide Luiz Schneider <oneidels@gmail.com>
2021-10-26 18:05:24 -0300

    add env and logs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.15
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"runtime"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Printf("Go version: %s\n", runtime.Version())
 	fmt.Println("Hello, World!")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
